77f7a9bf1aec0d9021fc8e4846ba0911aa755402
Dr. Jochen Manns <J.Manns@zera.de>
2024-01-03 17:00:18 +0100

    Added evaluation tests.

--- IronBlock.Tests/ExampleTests.cs
+++ IronBlock.Tests/ExampleTests.cs
@@ -10,16 +10,31 @@ namespace IronBlock.Tests
     public void Test_Example1_Xml()
     {
       var xml = File.ReadAllText("../../../Examples/example1.xml");
+
       var parser = Parser.CreateXml();
 
       parser.AddStandardBlocks();
-      var printer = parser.AddDebugPrinter();
+      parser.AddDebugPrinter();
 
       parser.Parse(xml).Evaluate();
 
       Assert.AreEqual("2,4,8,16", string.Join(",", TestExtensions.GetDebugText()));
     }
 
+    [TestMethod]
+    public void Test_Example1_Json()
+    {
+      var json = File.ReadAllText("../../../Examples/example1.json");
+
+      var parser = Parser.CreateJson();
+
+      parser.AddStandardBlocks();
+      parser.AddDebugPrinter();
+
+      parser.Parse(json).Evaluate();
+
+      Assert.AreEqual("2,4,8,16", string.Join(",", TestExtensions.GetDebugText()));
+    }
 
     [TestMethod]
     public void Test_Example2_Xml()
@@ -28,18 +43,26 @@ namespace IronBlock.Tests
       var parser = Parser.CreateXml();
 
       parser.AddStandardBlocks();
-      var printer = parser.AddDebugPrinter();
+      parser.AddDebugPrinter();
 
       parser.Parse(xml).Evaluate();
 
       Assert.AreEqual("Don't panic", TestExtensions.GetDebugText().First());
     }
 
+    [TestMethod]
+    public void Test_Example2_Json()
+    {
+      var json = File.ReadAllText("../../../Examples/example2.json");
 
+      var parser = Parser.CreateJson();
 
+      parser.AddStandardBlocks();
+      parser.AddDebugPrinter();
 
+      parser.Parse(json).Evaluate();
 
+      Assert.AreEqual("Don't panic", TestExtensions.GetDebugText().First());
+    }
   }
-
-
 }
